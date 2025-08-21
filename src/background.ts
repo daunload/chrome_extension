fa41bfd04513a925099a3a49f1e7ac3f56f32029
daunload <daun4799@gmail.com>
2025-08-21 14:35:05 +0900

    뱃지 노출 기능 추가

--- src/background.ts
+++ src/background.ts
@@ -4,4 +4,20 @@ console.log('Hello from the background!');
 
 browser.runtime.onInstalled.addListener((details) => {
 	console.log('Extension installed:', details);
+	showRedDot();
 });
+
+function showRedDot() {
+	chrome.action.setBadgeBackgroundColor({ color: '#d00' });
+	chrome.action.setBadgeText({ text: '•' });
+
+	if (chrome.action.setBadgeTextColor) {
+		chrome.action.setBadgeTextColor({ color: '#fff' });
+	}
+}
+
+function clearBadge() {
+	chrome.action.setBadgeText({ text: '' });
+}
+
+showRedDot();
